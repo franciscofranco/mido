0dfa2f874bc7d6e16d121b610ec46ab2a1e89791
Francisco Franco <franciscofranco.1990@gmail.com>
2018-09-17 02:00:11 +0000

    Revert "msm: ipa3: Fix redundant wakelock releases in error paths"
    We don't use ipa3.
    
    This reverts commit ca759f1a90d6f1550f6f53532d04e69976664dcb.

--- drivers/platform/msm/ipa/ipa_v3/ipa_dp.c
+++ drivers/platform/msm/ipa/ipa_v3/ipa_dp.c
@@ -262,6 +262,7 @@ static void ipa3_tx_switch_to_intr_mode(struct ipa3_sys_context *sys)
 
 	if (ipa3_ctx->transport_prototype == IPA_TRANSPORT_TYPE_GSI) {
 		atomic_set(&sys->curr_polling_state, 0);
+		ipa3_dec_release_wakelock();
 		ret = gsi_config_channel_mode(sys->ep->gsi_chan_hdl,
 			GSI_CHAN_MODE_CALLBACK);
 		if (ret != GSI_STATUS_SUCCESS) {
@@ -289,8 +290,8 @@ static void ipa3_tx_switch_to_intr_mode(struct ipa3_sys_context *sys)
 		}
 		atomic_set(&sys->curr_polling_state, 0);
 		ipa3_handle_tx_core(sys, true, false);
+		ipa3_dec_release_wakelock();
 	}
-	ipa3_dec_release_wakelock();
 	return;
 
 fail:
@@ -911,6 +912,7 @@ static void ipa3_rx_switch_to_intr_mode(struct ipa3_sys_context *sys)
 			goto fail;
 		}
 		atomic_set(&sys->curr_polling_state, 0);
+		ipa3_dec_release_wakelock();
 		ret = gsi_config_channel_mode(sys->ep->gsi_chan_hdl,
 			GSI_CHAN_MODE_CALLBACK);
 		if (ret != GSI_STATUS_SUCCESS) {
@@ -947,8 +949,8 @@ static void ipa3_rx_switch_to_intr_mode(struct ipa3_sys_context *sys)
 		}
 		atomic_set(&sys->curr_polling_state, 0);
 		ipa3_handle_rx_core(sys, true, false);
+		ipa3_dec_release_wakelock();
 	}
-	ipa3_dec_release_wakelock();
 	return;
 
 fail:
